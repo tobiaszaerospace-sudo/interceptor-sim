4ec56a30e0cbd5e8d8a8b6f171d269ad94c1cbb1
Anthony Tobiasz <tobiasz.aerospace@gmail.com>
2026-05-08 21:53:34 -0400

    Added settings menu to make everything more modular, also starting main.py

---
+++ Config/settings.py
@@ -0,0 +1,13 @@
+#SET UP GLOBAL SETTINGS THAT CAN BE CHANGED LATER - MAKES EVERYTHING MORE MODULAR AND FLEXIBLE
+class Settings:
+    def __init__(self):
+        #CAMERA SETTINGS
+        self.camera_fov_x = 70.0
+        self.image_width = 640
+        self.image_height = 480
+        self.camera_index = 0
+        #HARDWARE SETTINGS
+        self.servo_port = "COM4"
+        self.servo_baud = 115200
+        #MODEL SETTINGS
+        self.yolo_model_path = "yolov8n.pt"
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-#PURPOSE IS TO BE THE MISSION CONTROL / MAIN OPERATOR
-#RUNS ALL OF THE OTHER FUNCTIOSN AND CODE
+#IMPORT LIBRARIES
+import time
+import cv2
+import numpy as np
+#IMPORT CLASSES
+from Sensors.camera import Camera
+from Sensors.tracking import Tracker
+from Detection_Data_Processing.prediction import Prediction
+from Detection_Data_Processing.los_computation import LOSComputer
+from Hardware.servo import ServoController
+from Config.settings import Settings
 
-#LOAD INITIAL STATE
-
-#RUN SIMULATION LOOP
-
-#CALL GUIDANCE
-
-#CALL DYNAMICS
-
-#LOG DATA
-
-#CALL VISUAL
+#MAKE OPTION FUNCTIONS
+#OPTION 1: TRACKING
+def run_gimbal_tracking():
+    settings = Settings()
+    print("Starting Gimbal Tracking...")
+    #INITIALIZE CLASSES
+    tracker = Tracker()
+    servo = ServoController(port = settings.servo_port, buad = settings.servo_baud)
