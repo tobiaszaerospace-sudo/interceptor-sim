b5df14d140fe1f426cf2c4b8a8c8ac0b5c61e1c1
Anthony Tobiasz <tobiasz.aerospace@gmail.com>
2026-05-14 11:07:57 -0400

    Model Comparison file

---
+++ Simulation/model_comparison.py
@@ -0,0 +1,27 @@
+from simulate import run_simulation
+
+def compare_guidance_modes(dt, t_max, kill_radius, max_accel, tau, N):
+    #SETUP AMOUNT OF MODES, AND INITIALIZE RESULTS AS BLANK
+    modes = ["PN", "APN", "ZEM"]
+    results = {}
+
+    #TABLE FORMAT
+    print("\n=== Guidance Law Comparison ===")
+    print(f"{'Mode':<6} | {'Miss (m)':<10} | {'Hit':<5} | {'Peak Accel':<12} | {'Avg Accel':<12} | {'t_final (s)':<10}")
+    print("-"*70)
+
+    #SIMULATION CALCULATIONS
+    for mode in modes:
+        out = run_simulation(mode, dt, t_max, kill_radius, max_accel, tau, N)
+        results[mode] = out
+
+        #SHOW SIMULATION RESULTS IN FORMAT
+        print(f"{mode:<6} | "
+              f"{out['miss_distance']:<10.3f} | "
+              f"{str(out['hit']):<5} | "
+              f"{out['peak_accel']:<12.3f} | "
+              f"{out['avg_accel']:<12.3f} | "
+              f"{out['t_final']:<10.3f}")
+        
+    print("\nComparison complete.\n")
+    return results
