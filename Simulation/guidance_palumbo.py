e76d35d535a2196ea04b0a71ad5c2f2e4c92d3b8
Anthony Tobiasz <tobiasz.aerospace@gmail.com>
2026-05-13 22:09:36 -0400

    Guidance formulas from paper applied to code

---
+++ Simulation/guidance_palumbo.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+#GUIDANCE CLASS FOR PN, APN, AND ZEM
+class Guidance:
+    #INITIALIZE VALUES
+    def __init__(self, N=3.0, N_zem = 3.0):
+        self.N = N #NAVIGATION CONSTANT FOR PN/APN
+        self.N_zem = N_zem #NAVIGATION CONSTANT FOR ZERO EFFORT MISS BASED GUIDANCE
+
+    #PURE PROPORTIONAL NAVIGATION (PURE PN)
+    def pn(self, r_hat, Vc, los_rate_vec):
+        #FORMULA FROM PAPER
+        return self.N*Vc*np.cross(los_rate_vec, r_hat)
+    
+    #AUGMENTED PROPORTIONAL NAVIGATION (APN)
+    def apn(self, r_hat, Vc, los_rate_vec, a_target):
+        #FORMULAS FROM PAPER
+        a_pn = self.pn(r_hat, Vc, los_rate_vec)
+        a_t_parallel = np.dot(a_target, r_hat) * r_hat
+        a_t_perp = a_target - a_t_parallel
+        #BASIC APN FORMULA, PURE PN PLUS ACCELERATION VALUE
+        return a_pn + a_t_perp
+    
+    #ZERO EFFORT MISS
+    def compute_zem(self, r_rel, v_rel, a_target, t_go):
+        return r_rel + v_rel * t_go + .5 * a_target * (t_go**2)
+
+    def zem_guidance(self, r_rel, v_rel, a_target, Vc, Range):
+        #AVOID OVERSHOOT OR CORRECTION AT INTERCEPTION POINT
+        if Range == 0 or Vc <= 0:
+            return np.zeros(3)
+        #TIME LEFT
+        t_go = Range/Vc
+        #ZEM VALUE
+        zem = self.compute_zem(r_rel, v_rel, a_target, t_go)
+
+        #ZERO EFFORT MISS GUIDANCE OUTPUT
+        return -self.N_zem * zem / (t_go**2)
